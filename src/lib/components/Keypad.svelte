96275cfd04f9b1a06791edd27569c803c8085219
Sergey Khoroshavin <skhoroshavin@gmail.com>
2023-09-23 22:35:12 +0200

    Initial UI version

---
+++ src/lib/components/Keypad.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    export let onClick: (v: number) => void
+</script>
+
+<div class="keypad">
+    <button on:click={() => {onClick(1)}} class="button">1</button>
+    <button on:click={() => {onClick(2)}} class="button">2</button>
+    <button on:click={() => {onClick(3)}} class="button">3</button>
+    <button on:click={() => {onClick(4)}} class="button">4</button>
+    <button on:click={() => {onClick(5)}} class="button">5</button>
+    <button on:click={() => {onClick(6)}} class="button">6</button>
+    <button on:click={() => {onClick(7)}} class="button">7</button>
+    <button on:click={() => {onClick(8)}} class="button">8</button>
+    <button on:click={() => {onClick(9)}} class="button">9</button>
+</div>
+
+<style>
+    .keypad {
+        width: 100%;
+    }
+
+    .button {
+        width: 33%;
+        font-size: large;
+        background: deepskyblue;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
     import {Game} from "$lib/game";
+    import Keypad from "$lib/components/Keypad.svelte";
 
     const game = new Game()
+
+    let value = game.value
 </script>
 
 <h1 class="head">Zahlen bis zum 10!</h1>
-{game.value}
+{value}
+<Keypad onClick={(v) => {game.tryAnswer(v); value = game.value}}/>
 
 <style lang="postcss">
     .head {
