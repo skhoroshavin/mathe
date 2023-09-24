be9942d999aca8b76727c7dae09c919fd2025d7c
Sergey Khoroshavin <skhoroshavin@gmail.com>
2023-09-24 17:49:57 +0200

    Better UI, feedback on mistake

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,37 @@
 <script lang="ts">
+    import {onMount} from "svelte";
     import {Game} from "$lib/game";
+    import Display from "$lib/components/Display.svelte";
     import Keypad from "$lib/components/Keypad.svelte";
 
     const game = new Game()
 
     let value = game.value
+    let display: Display
+
+    onMount(() => {
+        game.onError = display.notifyError
+    })
 </script>
 
-<h1 class="head">Zahlen bis zum 10!</h1>
-{value}
-<Keypad onClick={(v) => {game.tryAnswer(v); value = game.value}}/>
+<div class="container">
+    <Display bind:this={display} value={value}/>
+    <Keypad onClick={(v) => {
+        game.tryAnswer(v);
+        value = game.value
+    }}/>
+</div>
 
-<style lang="postcss">
-    .head {
-        background: var(--primary-color);
+<style>
+    .container {
+        width: 100%;
+    }
+
+    @media (min-width: 640px) {
+        .container {
+            width: 640px;
+            margin-left: auto;
+            margin-right: auto;
+        }
     }
 </style>
