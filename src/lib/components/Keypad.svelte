bf0690ef64c98918e14fe40f6f05c204321c38a5
Sergey Khoroshavin <skhoroshavin@gmail.com>
2023-09-24 22:43:06 +0200

    Improve UI

--- src/lib/components/Keypad.svelte
+++ src/lib/components/Keypad.svelte
@@ -1,42 +1,35 @@
 <script lang="ts">
+    import Button from "$lib/components/Button.svelte";
+
     export let onClick: (v: number) => void
 </script>
 
 <div class="keypad">
     <div class="keypad-row">
-        <button on:click={() => {onClick(1)}} class="button">1</button>
-        <button on:click={() => {onClick(2)}} class="button">2</button>
-        <button on:click={() => {onClick(3)}} class="button">3</button>
+        <Button value={1} onClick={onClick}/>
+        <Button value={2} onClick={onClick}/>
+        <Button value={3} onClick={onClick}/>
     </div>
     <div class="keypad-row">
-        <button on:click={() => {onClick(4)}} class="button">4</button>
-        <button on:click={() => {onClick(5)}} class="button">5</button>
-        <button on:click={() => {onClick(6)}} class="button">6</button>
+        <Button value={4} onClick={onClick}/>
+        <Button value={5} onClick={onClick}/>
+        <Button value={6} onClick={onClick}/>
     </div>
     <div class="keypad-row">
-        <button on:click={() => {onClick(7)}} class="button">7</button>
-        <button on:click={() => {onClick(8)}} class="button">8</button>
-        <button on:click={() => {onClick(9)}} class="button">9</button>
+        <Button value={7} onClick={onClick}/>
+        <Button value={8} onClick={onClick}/>
+        <Button value={9} onClick={onClick}/>
     </div>
 </div>
 
 <style>
     .keypad {
         display: block;
+        padding: calc(1.5 * var(--vw));
     }
 
     .keypad-row {
         display: flex;
         width: 100%;
-    }
-
-    .button {
-        display: block;
-        flex-grow: 1;
-        background: mediumspringgreen;
-        font-size: 4rem;
-        margin: 0.25rem;
-        padding: 0.5rem;
-        border-radius: 0.5rem;
     }
 </style>
