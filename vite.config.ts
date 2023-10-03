0cb32f76503ef4eff4e36675a3033a5f9ca20e85
Sergey Khoroshavin <skhoroshavin@gmail.com>
2023-10-03 17:00:22 +0200

    Fix baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import {svelte} from '@sveltejs/vite-plugin-svelte'
 import {defineConfig} from "vitest/config";
 
 export default defineConfig({
+    base: process.env.BASE_URL,
     plugins: [svelte()],
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}']
