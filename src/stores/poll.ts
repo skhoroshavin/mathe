af8f5e6196e37e9ec518e9868e8d8ee515db1094
Sergey Khoroshavin <skhoroshavin@gmail.com>
2023-10-06 00:21:53 +0200

    Add events stream

--- src/stores/poll.ts
+++ src/stores/poll.ts
@@ -1,34 +1,11 @@
-import type {Subscriber} from "svelte/store";
-import type {StopUpdater} from "../core/updater"
 import {startUpdater} from "../core/updater";
+import {observable} from "./observable";
 
 export function poll<T>(fn: () => T) {
-    const subscribers = new Set<Subscriber<T>>()
-    let stopUpdater: StopUpdater
-
-    function notify() {
-        const value = fn()
-        for(const sub of subscribers) {
-            sub(value)
-        }
-    }
-
-    return {
-        subscribe: (sub: Subscriber<T>) => {
-            sub(fn())
-
-            subscribers.add(sub)
-            if (subscribers.size == 1) {
-                stopUpdater = startUpdater(notify)
-            }
-
-            return () => {
-                subscribers.delete(sub)
-                if (subscribers.size == 0) {
-                    console.log("Stop updater")
-                    stopUpdater()
-                }
-            }
-        }
-    }
+    return observable((emit) => {
+        emit(fn())
+        return startUpdater(() => {
+            emit(fn())
+        })
+    })
 }
