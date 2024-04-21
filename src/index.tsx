8f4297d17e14a38f2651615468aeb9406d43fb27
Sergey Khoroshavin <skhoroshavin@gmail.com>
2024-04-21 16:47:02 +0200

    Fix build?

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { render } from 'solid-js/web'
 
 import './normalize.css'
 import './index.css'
-import App from "./App";
+import App from "./app";
 
 const root = document.getElementById('root')
 
