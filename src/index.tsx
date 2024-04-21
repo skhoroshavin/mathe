68a1c6f447a4e9c4748f194fee980dddc97fade5
Sergey Khoroshavin <skhoroshavin@gmail.com>
2024-04-21 14:20:01 +0200

    Fix CI

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { render } from 'solid-js/web'
 
 import './normalize.css'
 import './index.css'
-import App from './App'
+import App from "./App.tsx";
 
 const root = document.getElementById('root')
 
